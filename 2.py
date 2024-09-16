3d2d1f7a9276f443b88e8e20c2152689f628d36e
QykXczj <116059420+QykXczj@users.noreply.github.com>
2024-09-17 02:38:17 +0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -14,6 +14,22 @@ run_command("git clone https://github.com/linkease/iStoreNAS.git")
 os.chdir("iStoreNAS")
 
 # 赋予权限
+run_command("chmod +x runmynas.sh")
+
+def create_expect_script():
+    expect_script = """
+#!/usr/bin/expect -f
+set timeout -1
+spawn ./runmynas.sh rk35xx
+expect eof
+"""
+    with open("runmynas.exp", "w") as file:
+        file.write(expect_script)
+
+# 创建 expect 脚本并赋予执行权限
+create_expect_script()
+
+# 赋予权限
 run_command("chmod +x runmynas.exp")
 
 # 执行 expect 脚本
